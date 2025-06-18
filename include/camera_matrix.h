ff90a6e495b74ad8d0cd5ed21c0993feecc49f1c
Truong Minh Khoi Nguyen <truongkhoizz95@gmail.com>
2025-06-18 16:20:13 +0200

    Unbrauchbare Daten wegwerfen

--- include/camera_matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAMERA_MATRIX_H
-#define CAMERA_MATRIX_H
-
-#include <GL/glew.h>
-
-void lookAt(GLfloat* out, const GLfloat* eye, const GLfloat* center, const GLfloat* up);
-void perspective(GLfloat* out, GLfloat fovy, GLfloat aspect, GLfloat near, GLfloat far);
-
-#endif
